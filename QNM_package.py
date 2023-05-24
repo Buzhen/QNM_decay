41588f9db04685fa77207e21b77f83f713c9255a
Eitan Tsuk <eitants@gmail.com>
2023-05-23 20:34:25 -0400

    Added a (failed) noise filteration

--- QNM_package.py
+++ QNM_package.py
@@ -45,11 +45,11 @@ def freq_for_mode(db, l=2, m=2, n=0, mass=1, spin=0, method="analytic"):
         assert False, "todo - add support for Leo Stein's code.\n"
     return freq_re + 1j*freq_im
 
-def props_from_freq(db, freq, l=2, m=2, n=0, method="analytic"):
+def props_from_freq(db, freq, l=2, m=2, n=0, method="LUT"):
     """
     This function extracts frequencies for a given mode using various methods
     1. Analytic interpolation presented in arXiv:gr-qc/0512160, eq. E1-E2.
-    2. Using look-up tables given in http://www.phy.olemiss.edu/ berti/qnms.html to the 0.1% level and interpolating in between.
+    2. Using look-up tables given in http://www.phy.olemiss.edu/ berti/qnms.html to the 0.1% level and interpolating.
     3. Using qnm Python package presented in arXiv:1908.10377
     """
     assert method in ["analytic", "LUT", "qnmpy"], "Method {0:s} is invalid.\n".format(method) # nana todo - add support for Leo Stein's code
@@ -81,11 +81,18 @@ def props_from_freq(db, freq, l=2, m=2, n=0, method="analytic"):
     #assert np.all(np.abs(freq-freq_for_mode(db, l=l, m=m, n=n, mass=mass, spin=spin)) < 1e-10)
     return mass, spin
 
-def freq_from_num_data(num_data):
+def freq_from_num_data(num_data, bDebug=False):
     amplitude = num_data.abs
     freq_im = -amplitude.derivative()/amplitude
     phase = num_data.arg_unwrapped
     freq_re = -phase.derivative()
+
+    if(bDebug):
+        h_plus = num_data.real
+        h_cross = num_data.imag
+        freq_im = (h_plus.derivative(2)*h_cross - h_cross.derivative(2)*h_plus)/(2*(h_cross.derivative()*h_plus - h_plus.derivative()*h_cross))
+        osc_plus = h_plus/np.exp(-freq_im*freq_im.t)
+        freq_re = np.sqrt(-osc_plus.derivative(2)/osc_plus)
     return freq_re + 1j*freq_im
 
 def fit_mode_amp_to_num_data(overtone_num, num_data, db, t0=0, l=2, m=2, mass=1, spin=0):
@@ -95,7 +102,27 @@ def fit_mode_amp_to_num_data(overtone_num, num_data, db, t0=0, l=2, m=2, mass=1,
     b_vec = np.array(num_data.data, ndmin=2).T
     for overtoneI in range(overtone_num):
         freqs[overtoneI] = freq_for_mode(db, l=l, m=m, n=overtoneI, mass=mass, spin=spin)
-        A_mat[:,overtoneI] = np.exp(-1j*freqs[overtoneI]*(num_data.t-num_data.t[0]+t0))
+        A_mat[:, overtoneI] = np.exp(-1j*freqs[overtoneI]*(num_data.t-num_data.t[0]+t0))
     assert not np.any(np.isnan(freqs))
     amps = np.linalg.inv(A_mat.T @ A_mat) @ A_mat.T @ b_vec
     return amps
+
+def clean_noise_freq(freq, fc, method="butterlowpass"):
+    # Clean numerical noise in the tail of the frequency (both real and imaginary parts
+
+    assert method in ["steplowpass", "butterlowpass", "running average"]
+
+    freq_re = np.real(freq)
+    freq_im = np.imag(freq)
+
+    fs = np.average(1/np.diff(freq.t))
+
+    # todo - make adaptive filter cutoff
+    if method=="butterlowpass":
+        b, a = scipy.signal.butter(8, fc, fs=fs, btype='low', analog=False)
+        freq_re_filt = scipy.signal.lfilter(b, a, freq_re)
+        freq_im_filt = scipy.signal.lfilter(b, a, freq_im)
+
+    freq_filtered = freq_re_filt + 1j*freq_im_filt
+
+    return freq_filtered
